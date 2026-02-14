56c18cd45d9848c15c5f8da57d524f60c3d050d0
kei67 <kei.02to.14@gmail.com>
2026-02-14 12:48:23 +0900

    actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/penguin-relationship/',
   plugins: [react()],
   resolve: {
     alias: {
